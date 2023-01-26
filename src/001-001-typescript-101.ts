836396ff1397a59cf6cb05c6faf8818fe53c7b0c
piggy <babymechanic@gmail.com>
2023-01-26 11:37:21 +1100

    initial commit

---
+++ src/001-001-typescript-101.ts
@@ -0,0 +1,35 @@
+export {};
+// types are optional on the right hand side if they can be inferred
+
+const value: string = 'hello world';
+
+// Interfaces and Types are interchangeable for objects but types for functions
+
+interface DoSomething {
+    doSomething(): Promise<void>;
+}
+
+type AlsoDoSomething = {
+    doSomething(): Promise<void>;
+}
+
+type Callable = () => Promise<void>;
+
+// typeof allows typescript to get meta information
+
+const apple = {name: 'apple', color: 'red'};
+type Fruit = typeof apple;
+const orange: Fruit = {color: 'orange', name: 'orange'};
+
+// keyof allows you to get all the keys of an object
+
+type FruitProp = keyof Fruit;
+
+const props: FruitProp[] = ['color', 'name'];
+
+// Most typed languages expect you to create a type before you assign data.
+// Typescripts real power comes from being able to compute types based on the data.
+
+const b: unknown = {greeting: 'hello'};
+
+
---
+++ src/002-001-string-enums-the-wrong-way.ts
@@ -0,0 +1,20 @@
+export {};
+
+type FruitName = 'apple' | 'orange';
+type FruitColor = 'orange' | 'red';
+
+type Fruit = {
+    colour: FruitColor;
+    name: FruitName;
+}
+
+const apple: Fruit = {
+    colour: 'red',
+    name: 'apple'
+}
+
+const orange: Fruit = {
+    colour: 'red',
+    name: 'apple'
+}
+
