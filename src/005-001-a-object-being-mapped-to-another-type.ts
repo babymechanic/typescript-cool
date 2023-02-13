9bc389bb1d30459661861215ebbabe58fbaeb1ce
piggy <babymechanic@gmail.com>
2023-02-13 17:31:33 +1100

    adding problem with creating types for a mapped type

---
+++ src/005-001-a-object-being-mapped-to-another-type.ts
@@ -0,0 +1,51 @@
+// common code
+export interface ParamType<T> {
+    parse(value: string): T;
+}
+
+const stringType: ParamType<string> = {
+    parse(value: string): string {
+        return value;
+    }
+};
+
+const numberType: ParamType<number> = {
+    parse(value: string): number {
+        return parseInt(value, 10);
+    }
+};
+
+function parseQueryParams<
+    TParams extends { [key: string]: ParamType<unknown> }
+>(queryDefinition: TParams, input: Partial<{ [key in keyof TParams]: string }>) {
+    type QueryDefinition = typeof queryDefinition;
+    type Keys = keyof QueryDefinition;
+    const keys = Object.keys(queryDefinition) as Keys[];
+    return keys.reduce((acc, key) => {
+        const rawValue = input[key];
+        acc[key] = rawValue == null ? rawValue : queryDefinition[key].parse(rawValue);
+        return acc;
+    }, {} as Partial<Record<Keys, unknown>>)
+}
+
+// end common code
+
+const searchFruitQueryParams = {
+    query: stringType,
+    colourId: numberType,
+}
+
+type SearchFruitQueryParams = {
+    query?: string;
+    colourId?: number;
+}
+
+const params = parseQueryParams(searchFruitQueryParams, {}) as SearchFruitQueryParams;
+
+/*
+in here we lose type checking by using an as
+we end up creating a type which might not be in sync with the actual input
+this type needs to be updated every time we make changes to `searchFruitQueryParams`
+*/
+
+
